f180f2a01d052cec989e651df97e76278e67b16d
JobCollins <dulo.collins@gmail.com>
2024-03-10 19:54:34 +0300

    Feature: Model Training Pipeline

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from awsec2_mlflow import logger
 from awsec2_mlflow.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from awsec2_mlflow.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from awsec2_mlflow.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from awsec2_mlflow.pipeline.stage_04_model_training import ModelTrainerPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -30,6 +31,17 @@ STAGE_NAME = "Data Transformation Stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Training Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainerPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
