b14728620ba181d522988d140b0eb080bd0a0147
JobCollins <dulo.collins@gmail.com>
2024-03-10 12:37:09 +0300

    Chore: Project Requirements and Setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "awsEC2_mlflow_mlops"
+AUTHOR_USER_NAME = "job"
+SRC_REPO = "awsEC2_mlflow_mlops"
+AUTHOR_EMAIL = "job.dulo@svrus.io"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for ml app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
