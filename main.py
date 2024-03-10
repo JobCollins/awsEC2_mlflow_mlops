bb740c789ae062aa0799f493bbfa0e63a3ef64d4
JobCollins <dulo.collins@gmail.com>
2024-03-10 13:53:38 +0300

    Feature: Data Ingestion
    data ingestion from source url and unzipping

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from awsec2_mlflow import logger
+from awsec2_mlflow.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to logging")
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
